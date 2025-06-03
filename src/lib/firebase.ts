1f5b0bdf35cb36cd86cac0fb4e6c686b7c8ea4c5
Tej Bahadur Thapa <164162889+Tejan195@users.noreply.github.com>
2025-06-04 01:49:07 +0530

    Clean up project structure and remove redundant files

--- src/lib/firebase.ts
+++
@@ -1,2 +0,0 @@
-// This file is intentionally empty as Firebase is not currently used in the project
-// If Firebase functionality is needed in the future, install the package and initialize it here
